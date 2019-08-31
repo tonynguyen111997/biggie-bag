29281b80bec854e95d731010ad7ac977813f55d1
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-08-31 16:45:18 -0700

    Add more enemies

--- js/app.js
+++ js/app.js
@@ -101,19 +101,37 @@ class Enemy extends Populate {
 }
 
 // (starting position, y position, speed)
+// Enemies on first 3 rows
 const enemy1 = new Enemy(201, 53, 550);
 const enemy2 = new Enemy(404, 136, 350);
 const enemy3 = new Enemy(0, 219, 675);
+const enemyMore1 = new Enemy(51, 53, 550);
+const enemyMore2 = new Enemy(204, 136, 350);
+const enemyMore3 = new Enemy(100, 219, 675);
+const enemyMoreMore1 = new Enemy(377, 53, 550);
+const enemyMoreMore2 = new Enemy(477, 136, 350);
+const enemyMoreMore3 = new Enemy(577, 219, 675);
 
+// Enemies on last 5 rows
 const enemy4 = new Enemy(0, 468, 500);
 const enemy5 = new Enemy(50, 551, 400);
 const enemy6 = new Enemy(320, 634, 300);
 const enemy7 = new Enemy(110, 717, 200);
 const enemy8 = new Enemy(220, 800, 350);
+const enemyMore4 = new Enemy(500, 468, 300);
+const enemyMore5 = new Enemy(400, 551, 600);
+const enemyMore6 = new Enemy(120, 634, 1000);
+const enemyMore7 = new Enemy(310, 717, 100);
+const enemyMore8 = new Enemy(520, 800, 250);
+const enemyMoreMore4 = new Enemy(300, 468, 100);
+const enemyMoreMore5 = new Enemy(100, 551, 200);
+const enemyMoreMore6 = new Enemy(420, 634, 100);
+const enemyMoreMore7 = new Enemy(210, 717, 300);
+const enemyMoreMore8 = new Enemy(120, 800, 550);
 
 allEnemies.push(
-  enemy1, enemy2, enemy3, 
-  enemy4, enemy5, enemy6, enemy7, enemy8);
+  enemy1, enemy2, enemy3, enemyMore1, enemyMore2, enemyMore3, enemyMoreMore1, enemyMoreMore2, enemyMoreMore3,
+  enemy4, enemy5, enemy6, enemy7, enemy8, enemyMore4, enemyMore5, enemyMore6, enemyMore7, enemyMore8);
 
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener("keyup", function (e) {
