7523f1c4ed4ab733ca3620337ddf82703f0b3043
jenniferafelton <justlittleoldmejenn@gmail.com>
2019-08-31 17:37:18 -0700

    hghghg

--- js/app.js
+++ js/app.js
@@ -1,76 +1,77 @@
 //Parent object for sprites
 class Populate {
-  constructor () {
-    this.x = 0;
-    this.y = 0;
-    this.speed = 0;
-    this.sprite = "";
-    this.sideways = 101;
-    this.upDown = 83;
-    this.scrollPosition = 500;
-  }
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.speed = 0;
+		this.sprite = '';
+		this.sideways = 101;
+		this.upDown = 83;
+		this.scrollPosition = 500;
+	}
 
-  render () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    window.scrollTo(0, this.scrollPosition);
-  }
+	render() {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+		window.scrollTo(0, this.scrollPosition);
+	}
 
-  reset () {
-    this.x = 505;
-    this.y = 883;
-    window.scrollTo(0, this.scrollPosition);
-  }
+	reset() {
+		this.x = 505;
+		this.y = 883;
+		window.scrollTo(0, this.scrollPosition);
+	}
 }
 
 //Player class
 class Player extends Populate {
-  constructor () {
-    super();
-    this.x = 505;
-    this.y = 883;
-    this.sprite = "images/char-boy.png";
-    this.scrollPosition = 500;
-  }
+	constructor() {
+		super();
+		this.x = 505;
+		this.y = 883;
+		this.sprite = 'images/char-boy.png';
+		this.scrollPosition = 500;
+	}
 
-//key input for Player
-  handleInput (input) {
-    switch (input) {
-      case "left":
-        if (this.x >= this.sideways) {
-          this.x -= this.sideways;
-        }
-        break;
-      case "right":
-        if (this.x <= this.sideways * 3) {
-          this.x += this.sideways;
-        }
-        break;
-      case "up":
-        if (this.y >= 0) {
-          this.y -= this.upDown;
-          this.scrollPosition -= 50;
-          window.scrollTo(0 , this.scrollPosition);
-        }
-        break;
-      case "down":
-        if (this.y <= this.upDown * 10) {
-          this.y += this.upDown;
-          this.scrollPosition += 50;
-          window.scrollTo(0 , this.scrollPosition);
-        }
-        break;
-    }
-  }
+	//key input for Player
+	handleInput(input) {
+		switch (input) {
+			case 'left':
+				if (this.x >= this.sideways) {
+					this.x -= this.sideways;
+				}
+				break;
+			case 'right':
+				if (this.x <= this.sideways * 3) {
+					this.x += this.sideways;
+				}
+				break;
+			case 'up':
+				if (this.y >= 0) {
+					this.y -= this.upDown;
+					this.scrollPosition -= 50;
+					window.scrollTo(0, this.scrollPosition);
+				}
+				break;
+			case 'down':
+				if (this.y <= this.upDown * 10) {
+					this.y += this.upDown;
+					this.scrollPosition += 50;
+					window.scrollTo(0, this.scrollPosition);
+				}
+				break;
+		}
+	}
 
-  //updates player and sets condition for collision & win
-  update () {
-    for (let enemy of allEnemies) {
-      if (this.y === enemy.y && (enemy.x + enemy.sideways / 2 > this.x && enemy.x < this.x + this.sideways / 2)) {
-        this.scrollPosition = 500;
-        this.reset();
-      }
-    }
-  }
+	//updates player and sets condition for collision & win
+	//I changed to divide by 5 instead of 2
+	update() {
+		for (let enemy of allEnemies) {
+			if (this.y === enemy.y && (enemy.x + enemy.sideways / 5 > this.x && enemy.x < this.x + this.sideways / 5)) {
+				this.scrollPosition = 500;
+				this.reset();
+			}
+		}
+	}
 }
 
 const player = new Player();
@@ -80,24 +81,24 @@ const allEnemies = [];
 
 //Enemy class
 class Enemy extends Populate {
-  constructor (x, y, speed) {
-    super();
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = "images/enemy-bug.png";
-    this.enemySprite = this.sprite;
-    this.sideways = 303;
-  }
+	constructor(x, y, speed) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.sprite = 'images/enemy-bug.png';
+		this.enemySprite = this.sprite;
+		this.sideways = 303;
+	}
 
-  //Smooth movement of Enemy objects across gameboard
-  update (dt) {
-    if (this.x < this.sideways * 5) {
-      this.x += this.speed * dt;
-    } else {
-      this.x = -100;
-    }
-  }
+	//Smooth movement of Enemy objects across gameboard
+	update(dt) {
+		if (this.x < this.sideways * 5) {
+			this.x += this.speed * dt;
+		} else {
+			this.x = -100;
+		}
+	}
 }
 
 // (starting position, y position, speed)
@@ -130,17 +131,35 @@ const enemyMoreMore7 = new Enemy(210, 717, 300);
 const enemyMoreMore8 = new Enemy(120, 800, 550);
 
 allEnemies.push(
-  enemy1, enemy2, enemy3, enemyMore1, enemyMore2, enemyMore3, enemyMoreMore1, enemyMoreMore2, enemyMoreMore3,
-  enemy4, enemy5, enemy6, enemy7, enemy8, enemyMore4, enemyMore5, enemyMore6, enemyMore7, enemyMore8);
+	enemy1,
+	enemy2,
+	enemy3,
+	enemyMore1,
+	enemyMore2,
+	enemyMore3,
+	enemyMoreMore1,
+	enemyMoreMore2,
+	enemyMoreMore3,
+	enemy4,
+	enemy5,
+	enemy6,
+	enemy7,
+	enemy8,
+	enemyMore4,
+	enemyMore5,
+	enemyMore6,
+	enemyMore7,
+	enemyMore8
+);
 
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener("keyup", function (e) {
-  var allowedKeys = {
-    37: "left",
-    38: "up",
-    39: "right",
-    40: "down"
-  };
+document.addEventListener('keyup', function(e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down',
+	};
 
-  player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
